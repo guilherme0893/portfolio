1c210fd383058ce71707e78b85db02c730cabd8a
Guilherme0893 <guilhermeduarte008@gmail.com>
2021-12-29 00:20:02 -0300

    add media query

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
-import Formulario from './pages/Formulario';
 import NotFound from './pages/NotFound';
-import repos from './data/repos';
+import MediaQuery from 'react-responsive';
 
 class App extends React.Component{
   constructor(props) {
@@ -38,28 +37,30 @@ class App extends React.Component{
     const {repositories, repoOnScreen} = this.state;
     return (
       <>
-        <Navbar 
-          repositories={repositories}
-          handleOnChange={this.handleOnChange}
-        />
-        <Switch>
-          <Route 
-            exact path="/portfolio"
-            component={Home} />
-          <Route 
-            path="/about"
-            render={(props) => 
-            <About name="name" {...props} />}
+        <MediaQuery minWidth={400}>
+          <Navbar 
+            repositories={repositories}
+            handleOnChange={this.handleOnChange}
           />
-          <Route
-            exact path="/projects"
-            render={() => <Portfolio repoOnScreen={repoOnScreen}/>}/>
-          <Route 
-            path="*"
-            component={NotFound}
-          />
-        </Switch>
-        <Footer />
+          <Switch>
+            <Route 
+              exact path="/portfolio"
+              component={Home} />
+            <Route 
+              path="/about"
+              render={(props) => 
+              <About name="name" {...props} />}
+            />
+            <Route
+              exact path="/projects"
+              render={() => <Portfolio repoOnScreen={repoOnScreen}/>}/>
+            <Route 
+              path="*"
+              component={NotFound}
+            />
+          </Switch>
+          <Footer />
+        </MediaQuery>
       </>      
     );
   }
