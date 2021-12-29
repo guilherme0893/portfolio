26a122a90c67c8b8c6e3a6933665c24930211bab
Guilherme0893 <guilhermeduarte008@gmail.com>
2021-12-28 22:08:38 -0300

    change route

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends React.Component{
         />
         <Switch>
           <Route 
-            exact path="/"
+            exact path="/portfolio"
             component={Home} />
           <Route 
             path="/about"
